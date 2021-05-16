007f9179270ea2cfb0001a0998a2439605e4c729
wangke <wannngkee@gmail.com>
2021-05-16 22:11:19 +0800

    add section three

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SectionOne from "./component/SectionOne/index";
 import SectionTwo from "./component/SectionTwo/index";
+import SectionThree from "./component/SectionThree/index";
 
 function App() {
   return (
     <div>
       <SectionOne />
       <SectionTwo />
+      <SectionThree />
     </div>
   );
 }
---
+++ src/component/SectionThree/index.jsx
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect, useState } from "react";
+import * as d3 from "d3";
+import "./index.css";
+
+const SectionThree = () => {
+  return (
+    <section className="sectionThree">
+      <div className="sectionTitle">
+        <h1>The Top 5 Fast Growing Industries</h1>
+        <p></p>
+      </div>
+      <div className="container">
+        <div className="left"></div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionThree;
