83af50d68b4477e8cc4d5e16da15ba2254eb465a
wangke <wannngkee@gmail.com>
2021-05-19 01:05:18 +0800

    update line chart, fix typo

--- src/component/SectionThree/index.jsx
+++ src/component/SectionThree/index.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
 import industry from "../../data/industry";
+import { FaRegHandPointDown } from "react-icons/fa";
 import "./index.css";
 
 const data = industry
@@ -17,17 +18,13 @@ const BarChart = () => {
   const [industry, setIndustry] = useState();
   const svgRef = useRef();
   useEffect(() => {
-    const margin = { top: 30, right: 0, bottom: 30, left: 30 };
+    const margin = { top: 30, right: 20, bottom: 30, left: 30 };
     const width = 450 - margin.left - margin.right;
     const barHeight = 70;
     const height =
       Math.ceil((data.length + 0.1) * barHeight) + margin.top + margin.bottom;
     const svg = d3.select(svgRef.current);
-    svg
-      .attr("width", 450)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    svg.attr("width", 450).attr("height", height).append("g");
 
     const xScale = d3
       .scaleLinear()
@@ -42,9 +39,8 @@ const BarChart = () => {
 
     svg
       .append("g")
-      .attr("class", "bar")
+      .attr("class", "industryBar")
       .attr("fill", "steelblue")
-      .style("opacity", 0.9)
       .selectAll("rect")
       .data(data)
       .join("rect")
@@ -52,16 +48,25 @@ const BarChart = () => {
       .attr("y", (d, i) => yScale(i))
       .attr("width", 0)
       .attr("height", yScale.bandwidth())
-      .on("mouseover", (e, d) => {
+      .on("mouseover", () => {
+        d3.select(".hint").style("opacity", 1);
+      })
+      .on("click", (e, d) => {
         let newLineData = lineData;
         d.entry.forEach((e, i) => {
           newLineData[i]["entry"] = e;
         });
-        d.exist.forEach((e, i) => {
-          newLineData[i]["exist"] = e;
+        d.exit.forEach((e, i) => {
+          newLineData[i]["exit"] = e;
         });
         setLineData(newLineData);
         setIndustry(d.industry);
+        d3.selectAll(".industryBar rect").attr("fill", "steelblue");
+        d3.select(e.target).attr("fill", "#325288");
+        d3.select(".lineChart").style("display", "block");
+      })
+      .on("mouseout", () => {
+        d3.select(".hint").style("opacity", 0);
       });
 
     svg
@@ -99,6 +104,13 @@ const BarChart = () => {
           .tickSizeOuter(0)
       );
     svg.select(".x-axis").call(xAxis);
+    svg
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("x", 60)
+      .attr("y", 0)
+      .text("Growth Rate")
+      .style("font-size", 10);
     svg.select(".y-axis").call(yAxis);
 
     // start animation when mouse hover on this section
@@ -116,12 +128,12 @@ const BarChart = () => {
   return (
     <>
       <div className="left">
-        <svg ref={svgRef}>
+        <svg ref={svgRef} className="barchart">
           <g className="x-axis" />
           <g className="y-axis" />
         </svg>
       </div>
-      <div className="right">
+      <div className="lineChart">
         <LineChart lineData={lineData} industry={industry} />
       </div>
     </>
@@ -160,10 +172,10 @@ const LineChart = (props) => {
         .y((d) => yScale(d.entry));
       // .curve(d3.curveMonotoneX);
 
-      const existPath = d3
+      const exitPath = d3
         .line()
         .x((d) => xScale(d.year))
-        .y((d) => yScale(d.exist));
+        .y((d) => yScale(d.exit));
       // .curve(d3.curveMonotoneX);
 
       svg
@@ -183,7 +195,7 @@ const LineChart = (props) => {
         .join("path")
         .attr("class", "line-path2")
         .attr("transform", "translate(" + 50 + ",0)")
-        .attr("d", existPath)
+        .attr("d", exitPath)
         .attr("fill", "none")
         .attr("stroke-width", 3)
         .attr("stroke", "orange");
@@ -239,8 +251,86 @@ const LineChart = (props) => {
         .attr("class", "text2")
         .attr("x", 90)
         .attr("y", height + margin.bottom + margin.top + 10)
-        .text("exist rate")
+        .text("exit rate")
         .style("font-size", "10px");
+
+      const focus = svg
+        .append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+
+      focus
+        .append("circle")
+        .attr("class", "y1")
+        .style("fill", "none")
+        .style("stroke", "red")
+        .attr("r", 4);
+
+      focus
+        .append("circle")
+        .attr("class", "y2")
+        .style("fill", "none")
+        .style("stroke", "red")
+        .attr("r", 4);
+
+      focus
+        .append("line")
+        .attr("class", "x-hover-line hover-line")
+        .attr("y1", 0)
+        .style("stroke", "#6F257F")
+        .style("stroke-width", 3)
+        .attr("y2", height);
+
+      // focus
+      //   .append("line")
+      //   .attr("class", "y-hover-line hover-line")
+      //   .attr("x1", 0)
+      //   .attr("x2", width / 2)
+      //   .attr("y", 20)
+      //   .style("stroke", "red")
+      //   .style("stroke-width", 3);
+
+      focus.append("text").attr("x", 15).attr("dy", ".31em");
+
+      svg
+        .append("rect")
+        .attr("class", "overlay")
+        .style("fill", "none")
+        .attr("width", width)
+        .attr("height", height + 10)
+        .style("opacity", 0)
+        .style("fill", "red")
+        .on("mouseover", () => {
+          console.log("over");
+          d3.select(".focus").style("display", "block");
+        })
+        .on("mouseout", () => {
+          focus.style("display", "none");
+        })
+        .on("mousemove", (e) => mousemove(e));
+
+      function mousemove(e) {
+        const eachBand = xScale.step();
+        const index = Math.round(e.x / eachBand) - 6;
+        console.log(index);
+        //  const val = xScale.domain()[index];
+        // const bisectX = d3.bisector(d => d[0]).left;
+        // const x0 = xScale.invert(d3.pointer(e)[0]),
+        //   i = bisectX(data, x0, 1),
+        //   d0 = data[i - 1],
+        //   d1 = data[i],
+        //   d = x0 - d0.year > d1.year - x0 ? d1 : d0;
+        // console.log(x0, i, d1, d);
+        // focus.attr(
+        //   "transform",
+        //   "translate(" + x(d.year) + "," + y(d.value) + ")"
+        // );
+        // focus.select("text").text(function () {
+        //   return d.value;
+        // });
+        focus.select(".x-hover-line").attr("y2", height);
+        focus.select(".y-hover-line").attr("x2", width + width);
+      }
     }
   }, [lineData, industry, length]);
   return (
@@ -258,7 +348,11 @@ const SectionThree = () => {
     <section className="sectionThree">
       <div className="sectionTitle">
         <h1>The Top 5 Thriving Industries</h1>
-        <p>See the trend from how industries grow</p>
+        <p>See how industries grow over the years</p>
+      </div>
+      <div className="hint">
+        <FaRegHandPointDown className="icon" />
+        Click for more details
       </div>
       <div className="containerThree">
         <BarChart />
