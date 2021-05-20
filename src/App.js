4f20f826875ca350acb422cdd85ca6f413eb5c1e
wangke <wannngkee@gmail.com>
2021-05-21 00:28:20 +0800

    add antd style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./App.css";
 import SectionOne from "./component/SectionOne/index";
 import SectionTwo from "./component/SectionTwo/index";
 import SectionThree from "./component/SectionThree/index";
 import SectionFour from "./component/SectionFour/index";
+// import "antd/dist/antd.css";
 
 function App() {
   return (
