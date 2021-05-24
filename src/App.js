8bf740f2d5e32c2fbc1ec9e28b903a69800d64b6
wangke <wannngkee@gmail.com>
2021-05-24 19:07:24 +0800

    add backTop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import { BackTop } from "antd";
+import { RiArrowUpSLine } from "react-icons/ri";
 import SectionOne from "./component/SectionOne/index";
 import SectionTwo from "./component/SectionTwo/index";
 import SectionThree from "./component/SectionThree/index";
 import SectionFour from "./component/SectionFour/index";
 import SectionFive from "./component/SectionFive/index";
-// import "antd/dist/antd.css";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
       <SectionThree />
       <SectionFour />
       <SectionFive />
+      <BackTop style={{ opacity: 0.8, marginRight: -60 }}>
+        <RiArrowUpSLine className="backTop" />
+      </BackTop>
     </div>
   );
 }
