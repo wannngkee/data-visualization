365bdcec0ff6f46b9d3151ef67ec8a5648b13377
wangke <wannngkee@gmail.com>
2021-06-04 10:40:03 +0800

    refactor(l): change img path

--- src/component/SectionOne/index.jsx
+++ src/component/SectionOne/index.jsx
@@ -1,14 +1,11 @@
 import React from "react";
+import backgroundImg from "./imgs/background.jpg";
 import "./index.css";
 
 const SectionOne = () => {
   return (
     <section className="sectionOne">
-      <img
-        className="backgroundImg"
-        src="imgs/background.jpg"
-        alt="melbourne city"
-      />
+      <img className="backgroundImg" src={backgroundImg} alt="melbourne city" />
       <div className="titleBlock">
         <h1>Plan Your Business in Melbourne</h1>
       </div>
